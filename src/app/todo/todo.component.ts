65e7fc98639371955a147f7cc2e79d91761f088e
PoukisaOufesa <nobody@cares.com>
2020-11-29 17:40:50 +0200

    Read implementation

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  private todos;
+  private activeTasks;
+
+  constructor(private todoService: TodoService) { }
+
+  getTodos(){
+    return this.todoService.get().then(todos => {
+      this.todos = todos;
+      this.activeTasks = this.todos.filter(todo => !todo.isDone).length;
+    });
+  }
 
   ngOnInit(): void {
+    this.getTodos();
   }
 
 }
