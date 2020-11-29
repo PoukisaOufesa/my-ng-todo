a450ab7ff05a678214714f3399496f9e1725f615
PoukisaOufesa <nobody@cares.com>
2020-11-29 21:26:04 +0200

    update functionality added

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -24,4 +24,14 @@ export class TodoService {
       resolve(data);
     });
   }
+
+  put(changed){
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === changed);
+      TODOS[index].title = changed.title;
+      resolve(changed);
+    }
+
+    )
+  }
 }
