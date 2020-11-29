b3fdb64da58eefa25151aa05568192369d86cc8a
PoukisaOufesa <nobody@cares.com>
2020-11-29 22:33:34 +0200

    Routing - 3

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -20,13 +20,13 @@ export class TodoComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.path = params['status'];
-      this.getTodos();
+      this.getTodos(this.path);
     });
 
   }
 
-  getTodos(){
-    return this.todoService.get().then(todos => {
+  getTodos(query = ''){
+    return this.todoService.get(query).then(todos => {
       this.todos = todos;
       this.activeTasks = this.todos.filter(todo => !todo.isDone).length;
     });
