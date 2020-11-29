b7c33c61f7981a457921e2ff9dbb8ac42b2cea69
PoukisaOufesa <nobody@cares.com>
2020-11-29 22:14:47 +0200

    Rounting - 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { FormsModule } from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http'
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: ':status', component: TodoComponent }, //:status - parameter what can take any value
+  { path: '**', redirectTo: '/all' } //** ** - is any other path */
+];  
 
 @NgModule({
   declarations: [
@@ -14,7 +23,9 @@ import { FormsModule } from '@angular/forms'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
